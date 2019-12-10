e47bf5f82c7c4118da4c29656719b5d6e6ed4ea5
AKHYARI - FX <17523207@students.uii.ac.id>
2019-12-11 03:32:07 +0700

    Sudah berhasil show data ke fragment

--- app/src/main/java/android/example/drventryca/HomeFragment.java
+++ app/src/main/java/android/example/drventryca/HomeFragment.java
@@ -10,23 +10,38 @@ import android.view.ViewGroup;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeFragment extends Fragment {
 
     private CardView klikDiag, klikData, klikFakta, berita1, berita2, berita3;
     private ImageView news;
     private PropertyValuesHolder scaleX, scaleY, aplha;
-    private TextView appTitle, halo, nama, kabar, bawah1, bawah2;
+    TextView appTitle, halo, etNama, kabar, bawah1, bawah2;
+
+    DatabaseReference reference;
+    FirebaseUser user;
+    FirebaseAuth auth;
+    View view;
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        view = inflater.inflate(R.layout.fragment_home, container, false);
 
 
          appTitle = view.findViewById(R.id.app_title);
@@ -36,7 +51,7 @@ public class HomeFragment extends Fragment {
          news = view.findViewById(R.id.news);
          TextView lihat_ = view.findViewById(R.id.lihat_tips_lain);
          halo = view.findViewById(R.id.halo);
-         nama = view.findViewById(R.id.nama);
+        etNama = view.findViewById(R.id.nama);
          kabar = view.findViewById(R.id.kabar);
          bawah1 = view.findViewById(R.id.text_bawah1);
          /*bawah2 = view.findViewById(R.id.text_bawah2);*/
@@ -45,9 +60,33 @@ public class HomeFragment extends Fragment {
          /*berita3 = view.findViewById(R.id.ber)*/
 
 
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+
+
+        reference = FirebaseDatabase.getInstance().getReference().child("User:").child(user.getUid());
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String namaDepan = dataSnapshot.child("namaDepan").getValue().toString();
+                etNama.setText(namaDepan);
+                Toast.makeText(getContext(), namaDepan, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+
          /*kabar.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.left_slide_in2));*/
          halo.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.left_slide_in_smooth));
-         nama.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.left_slide_in_smooth));
+        etNama.setAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.left_slide_in_smooth));
          /*appTitle.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.showed_up));*/
          /*news.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.left_slide_in));*/
          klikDiag.setAnimation(AnimationUtils.loadAnimation(getContext(),R.anim.translate_y_show3));
@@ -103,6 +142,7 @@ public class HomeFragment extends Fragment {
 
          return view;
     }
+
 
     float x1,x2,y1,y2;
 
