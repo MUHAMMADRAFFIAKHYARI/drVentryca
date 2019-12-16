8071b3290f29aed850fd6fa07e099fccac13f7b0
fathonihirzin <hirzinfathoni@gmail.com>
2019-12-17 02:57:13 +0700

    Interpretasi Next Level [Perut, Tidak Mual & Tidak Muntah] Done

--- app/src/main/java/android/example/drventryca/Interpretasi/InterpretasiDiagPer3_1.java
+++ app/src/main/java/android/example/drventryca/Interpretasi/InterpretasiDiagPer3_1.java
@@ -12,6 +12,12 @@ public class InterpretasiDiagPer3_1 {
     private int position;
     private DiseaseModel diseaseModel;
 
+    private int hitungP1 = 3;
+    private int hitungP2 = 3;
+    private int hitungP3 = 3;
+    private int hitungP4 = 3;
+    private int hitungP5 = 3;
+
 
     public InterpretasiDiagPer3_1(ArrayList<QuestionModel> questionModels){
         this.questionModels = questionModels;
@@ -34,7 +40,7 @@ public class InterpretasiDiagPer3_1 {
         position = questionModels.size();
 
 
-        for (int i = 0; i < position; i++) {
+        /*for (int i = 0; i < position; i++) {
 
             if (questionModels.get(0).isAnswer() == true &&
                     questionModels.get(1).isAnswer() == true &&
@@ -148,6 +154,194 @@ public class InterpretasiDiagPer3_1 {
             else{
                 setDiagnosis("Belum Dapat di Diagnosa, silahkan periksakan diri anda ke dokter");
             }
+        }*/
+
+        if (questionModels.get(0).isAnswer() == true) {
+            hitungP2 += 1;
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(1).isAnswer() == true) {
+            hitungP2 += 1;
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(2).isAnswer() == true) {
+            hitungP2 += 1;
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(3).isAnswer() == true) {
+            hitungP5 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(4).isAnswer() == true) {
+            hitungP1 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(5).isAnswer() == true) {
+            hitungP1 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(6).isAnswer() == true) {
+            hitungP1 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(7).isAnswer() == true) {
+            hitungP1 += 1;
+            hitungP2 += 1;
+            hitungP3 += 1;
+            hitungP5 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(8).isAnswer() == true) {
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(9).isAnswer() == true) {
+            hitungP2 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(10).isAnswer() == true) {
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        if (questionModels.get(11).isAnswer() == true) {
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+            Log.d("hitung p5", String.valueOf(hitungP5));
+        }
+
+        else {
+            setDiagnosis("Belum Dapat di Diagnosa, silahkan periksakan diri anda ke dokter");
+        }
+
+        Log.d("hitung p1 akhir", String.valueOf(hitungP1));
+        Log.d("hitung p2 akhir", String.valueOf(hitungP2));
+        Log.d("hitung p3 akhir", String.valueOf(hitungP3));
+        Log.d("hitung p4 akhir", String.valueOf(hitungP4));
+        Log.d("hitung p5 akhir", String.valueOf(hitungP5));
+
+        if (hitungP1 > hitungP2 && hitungP1 > hitungP3 && hitungP1 > hitungP4 && hitungP1 > hitungP5) {
+            setDiagnosis("Whipple\n\n Anda memenuhi " + hitungP1 + " dari 8 gejala yang ada");
+        } else if (hitungP2 > hitungP1 && hitungP2 > hitungP3 && hitungP2 > hitungP4 && hitungP2 > hitungP5) {
+            setDiagnosis("Traveller's Diarrhea\n\n Anda memenuhi " + hitungP2 + " dari 9 gejala yang ada");
+        } else if (hitungP3 > hitungP1 && hitungP3 > hitungP2 && hitungP3 > hitungP4 && hitungP3 > hitungP5) {
+            setDiagnosis("Diarre Akut\n\n Anda memenuhi " + hitungP3 + " dari 7 gejala yang ada");
+        } else if (hitungP4 > hitungP1 && hitungP4 > hitungP2 && hitungP4 > hitungP3 && hitungP4 > hitungP5) {
+            setDiagnosis("Diare Kronik\n\n Anda memenuhi " + hitungP4 + " dari 8 gejala yang ada");
+        } else if (hitungP5 > hitungP1 && hitungP5 > hitungP2 && hitungP5 > hitungP5 && hitungP5 > hitungP1) {
+            setDiagnosis("Divertikulosis\n\n Anda memenuhi " + hitungP5 + " dari 5 gejala yang ada");
+        }
+
+        else if (hitungP1 == hitungP2 && hitungP1!=3) {
+            setDiagnosis("Whipple, anda memenuhi " + hitungP1 + " dari 8 gejala yang ada \n" +
+                    "Traveller's Diarrhea, anda memenuhi " + hitungP2 + " dari 9 gejala yang ada");
+        }
+        else if (hitungP1 == hitungP3 && hitungP1!=3) {
+            setDiagnosis("Whipple, anda memenuhi " + hitungP1 + " dari 8 gejala yang ada \n" +
+                    "Diarre Akut, anda memenuhi " + hitungP3 + " dari 7 gejala yang ada");
+        }
+
+        else if (hitungP1 == hitungP4 && hitungP1!=3) {
+            setDiagnosis("Whipple, anda memenuhi " + hitungP1 + " dari 8 gejala yang ada \n" +
+                    "Diarre Kronik, anda memenuhi " + hitungP4 + " dari 8 gejala yang ada");
+        }
+
+        else if (hitungP1 == hitungP5 && hitungP1!=3) {
+            setDiagnosis("Whipple, anda memenuhi " + hitungP1 + " dari 8 gejala yang ada \n" +
+                    "Divertikulosis, anda memenuhi " + hitungP5 + " dari 5 gejala yang ada");
+        }
+
+        else if (hitungP2 == hitungP3 && hitungP2!=3) {
+            setDiagnosis("Traveller's Diarrhea, anda memenuhi " + hitungP2 + " dari 9 gejala yang ada \n" +
+                    "Diarre Akut, anda memenuhi " + hitungP3 + " dari 7 gejala yang ada");
+        }
+
+        else if (hitungP2 == hitungP4 && hitungP2!=3) {
+            setDiagnosis("Traveller's Diarrhea, anda memenuhi " + hitungP2 + " dari 9 gejala yang ada \n" +
+                    "Diarre Kronik, anda memenuhi " + hitungP4 + " dari 8 gejala yang ada");
+        }
+
+        else if (hitungP2 == hitungP5 && hitungP2!=3) {
+            setDiagnosis("Traveller's Diarrhea, anda memenuhi " + hitungP2 + " dari 9 gejala yang ada \n" +
+                    "Divertikulosis, anda memenuhi " + hitungP5 + " dari 5 gejala yang ada");
+        }
+
+        else if (hitungP3 == hitungP4 && hitungP3!=3) {
+            setDiagnosis("Diarre Akut, anda memenuhi " + hitungP3 + " dari 7 gejala yang ada \n" +
+                    "Diarre Kronik, anda memenuhi " + hitungP4 + " dari 8 gejala yang ada");
+        }
+
+        else if (hitungP4 == hitungP5 && hitungP4!=3) {
+            setDiagnosis("Diarre Kronik, anda memenuhi " + hitungP4 + " dari 8 gejala yang ada \n" +
+                    "Divertikulosis, anda memenuhi " + hitungP5 + " dari 5 gejala yang ada");
+        }
+
+        else {
+            setDiagnosis("Kami kekurangan data untuk mendiognosa keluhan anda, silahkan masukkan gejala dengan benar ^-^");
         }
     };
 }
--- app/src/main/java/android/example/drventryca/Interpretasi/InterpretasiDiagPer3_2.java
+++ app/src/main/java/android/example/drventryca/Interpretasi/InterpretasiDiagPer3_2.java
@@ -12,6 +12,11 @@ public class InterpretasiDiagPer3_2 {
     private int position;
     private DiseaseModel diseaseModel;
 
+    private int hitungP1 = 2;
+    private int hitungP2 = 2;
+    private int hitungP3 = 2;
+    private int hitungP4 = 2;
+
 
     public InterpretasiDiagPer3_2(ArrayList<QuestionModel> questionModels){
         this.questionModels = questionModels;
@@ -34,7 +39,7 @@ public class InterpretasiDiagPer3_2 {
         position = questionModels.size();
 
 
-        for (int i = 0; i < position; i++) {
+        /*for (int i = 0; i < position; i++) {
 
             if (questionModels.get(0).isAnswer() == true &&
                     questionModels.get(1).isAnswer() == true &&
@@ -135,6 +140,169 @@ public class InterpretasiDiagPer3_2 {
             else{
                 setDiagnosis("Belum Dapat di Diagnosa, silahkan periksakan diri anda ke dokter");
             }
+        }*/
+
+        if (questionModels.get(0).isAnswer() == true) {
+            hitungP1 += 1;
+            hitungP2 += 1;
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(1).isAnswer() == true) {
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(2).isAnswer() == true) {
+            hitungP1 += 1;
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(3).isAnswer() == true) {
+            hitungP1 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(4).isAnswer() == true) {
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(5).isAnswer() == true) {
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(6).isAnswer() == true) {
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(7).isAnswer() == true) {
+            hitungP3 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(8).isAnswer() == true) {
+            hitungP3 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(9).isAnswer() == true) {
+            hitungP3 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(10).isAnswer() == true) {
+            hitungP2 += 1;
+            hitungP4 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(11).isAnswer() == true) {
+            hitungP2 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        if (questionModels.get(12).isAnswer() == true) {
+            hitungP3 += 1;
+            Log.d("hitung p1", String.valueOf(hitungP1));
+            Log.d("hitung p2", String.valueOf(hitungP2));
+            Log.d("hitung p3", String.valueOf(hitungP3));
+            Log.d("hitung p4", String.valueOf(hitungP4));
+        }
+
+        else {
+            setDiagnosis("Belum Dapat di Diagnosa, silahkan periksakan diri anda ke dokter");
+        }
+
+
+        Log.d("hitung p1 akhir", String.valueOf(hitungP1));
+        Log.d("hitung p2 akhir", String.valueOf(hitungP2));
+        Log.d("hitung p3 akhir", String.valueOf(hitungP3));
+        Log.d("hitung p4 akhir", String.valueOf(hitungP4));
+
+        if (hitungP1 > hitungP2 && hitungP1 > hitungP3 && hitungP1 > hitungP4) {
+            setDiagnosis("Kolitis Ulserativa\n\n Anda memenuhi " + hitungP1 + " dari 5 gejala yang ada");
+        } else if (hitungP2 > hitungP1 && hitungP2 > hitungP3 && hitungP2 > hitungP4) {
+            setDiagnosis("Amubiasis\n\n Anda memenuhi " + hitungP2 + " dari 8 gejala yang ada");
+        } else if (hitungP3 > hitungP1 && hitungP3 > hitungP2 && hitungP3 > hitungP4) {
+            setDiagnosis("Seliak\n\n Anda memenuhi " + hitungP3 + " dari 8 gejala yang ada");
+        } else if (hitungP4 > hitungP1 && hitungP4 > hitungP2 && hitungP4 > hitungP3) {
+            setDiagnosis("Irritable Bowel Syndrome\n\n Anda memenuhi " + hitungP4 + " dari 7 gejala yang ada");
+        }
+        else if (hitungP1 == hitungP2 && hitungP1!=2) {
+            setDiagnosis("Kolitis Ulserativa " + hitungP1 + " dari 5 gejala yang ada \n" +
+                    "Amubiasis, anda memenuhi " + hitungP2 + " dari 8 gejala yang ada");
+        }
+        else if (hitungP1 == hitungP3 && hitungP1!=2) {
+            setDiagnosis("Kolitis Ulserativa " + hitungP1 + " dari 5 gejala yang ada \n" +
+                    "Seliak, anda memenuhi " + hitungP3 + " dari 8 gejala yang ada");
+        }
+
+        else if (hitungP1 == hitungP4 && hitungP1!=2) {
+            setDiagnosis("Kolitis Ulserativa " + hitungP1 + " dari 5 gejala yang ada \n" +
+                    "Irritable Bowel Syndrome, anda memenuhi " + hitungP4 + " dari 7 gejala yang ada");
+        }
+
+        else if (hitungP2 == hitungP3 && hitungP2!=2) {
+            setDiagnosis("Amubiasis " + hitungP2 + " dari 8 gejala yang ada \n" +
+                    "Seliak " + hitungP3 + " dari 8 gejala yang ada");
+        }
+
+        else if (hitungP2 == hitungP4 && hitungP2!=2) {
+            setDiagnosis("Amubiasis " + hitungP2 + " dari 8 gejala yang ada \n" +
+                    "Irritable Bowel Syndrome, anda memenuhi " + hitungP4 + " dari 7 gejala yang ada");
+        }
+
+        else if (hitungP3 == hitungP4 && hitungP3!=2) {
+            setDiagnosis("Seliak, anda memenuhi " + hitungP3 + " dari 8 gejala yang ada \n" +
+                    "Irritable Bowel Syndrome, anda memenuhi " + hitungP4 + " dari 7 gejala yang ada");
+        }
+
+        else {
+            setDiagnosis("Kami kekurangan data untuk mendiognosa keluhan anda, silahkan masukkan gejala dengan benar ^-^");
         }
     };
 }
